fb7cf8e91216e973b46bd6f8df63f6e00742114f
Hp de moa <taquettom3@gmail.com>
2026-03-17 17:07:38 +0100

    + | Added -rs

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include "libft.h"
 
 #define RESET		"\x1b[0m"
@@ -27,32 +28,32 @@
 #define BOLD		"\x1b[1m"
 #define UNDERLINE	"\x1b[4m"
 
-#define FLAG_LIST "adglrstuR"
-
 enum {
 	FLAG_A	 = 1u << 0,  /* -a : show hidden files */
 	FLAG_D	 = 1u << 1,  /* -d : list directories themselves */
-	FLAG_G	 = 1u << 2,  /* -g : (unused / group) */
+	// FLAG_G	 = 1u << 2,  /* -g : (unused / group) */
 	FLAG_L	 = 1u << 3,  /* -l : long format */
-	FLAG_S	 = 1u << 4,  /* -s : show block size */
+	FLAG_R = 1u << 4,  /* -r : reverse sort */
 	FLAG_R_CAP = 1u << 5,  /* -R : recursive */
-	FLAG_R = 1u << 6,  /* -r : reverse sort */
+	FLAG_S	 = 1u << 6,  /* -s : show block size */
+	// FLAG_S_CAP = 1u << 7, /* -S : Sort by size */
 };
 
-struct flags {
+struct env {
 	uint32_t flags_mask;
+	bool stat;
 };
 
 struct filesData {
 	char			name[256];
 	unsigned char	type;
-	struct stat		stat;
+	struct stat		*stat;
 };
 
 /* prototypes */
-void	filePrinter(char name[256], unsigned char type);
-bool	handleFlags(char *flags, struct flags *tflags);
-void	initfList(struct flags *flags);
+void	filesPrinter(struct filesData files[500], uint32_t flags_mask, int last);
+bool	handleFlags(char *flags, struct env *tflags);
+void	initfList(struct env *flags);
 
 //struct dirent {
 //	ino_t		  d_ino;	   /* Inode number */
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 15:30:48 by tom               #+#    #+#             */
-/*   Updated: 2026/03/17 15:32:20 by tom              ###   ########.fr       */
+/*   Updated: 2026/03/17 17:07:11 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,68 @@
 static const uint32_t FLAG_MAP[256] = {
 	['a'] = FLAG_A,
 	['d'] = FLAG_D,
-	['g'] = FLAG_G,
+	// ['g'] = FLAG_G,
 	['l'] = FLAG_L,
 	['s'] = FLAG_S,
 	['R'] = FLAG_R_CAP,
 	['r'] = FLAG_R,
 };
 
-static inline int flags_is_set(uint32_t mask, unsigned char c) {
+static inline int flagIsSet(uint32_t mask, unsigned char c) {
 	return (mask & FLAG_MAP[c]) != 0;
 }
 
-void filePrinter(char name[256], unsigned char type) {
-	if (type == 4)
-		printf("\x1b[1m\x1b[34m%s  \x1b[0m", name);
-	else 
-		printf("%s  ", name);
+void printLine(uint32_t flags_mask, struct filesData files) {
+	if (flagIsSet(flags_mask, 's')) ft_printf("  %d ", (files.stat->st_blocks * 512 + 1023) / 1024);
+	if (files.type == 4) ft_printf("\x1b[1m\x1b[34m%s  \x1b[0m", files.name);
+	else ft_printf("%s  ", files.name);
 }
 
-bool handleFlags(char *flags, struct flags *tflags) {
+void filesPrinter(struct filesData files[500], uint32_t flags_mask, int last) {
+	if (flagIsSet(flags_mask, 'l') || flagIsSet(flags_mask, 's'))
+		ft_printf("total %d\n", 123);
+	if (flagIsSet(flags_mask, 'r')) {
+		last--;
+		for (; last >= 0; last--){
+			printLine(flags_mask, files[last]);
+		}
+	} else {
+		for (int k = 0; k < last; k++){
+			printLine(flags_mask, files[k]);
+		}
+	}
+}
+
+bool handleFlags(char *flags, struct env *tflags) {
 	for (int i = 1; flags[i]; i++) {
 		unsigned char c = (unsigned char)flags[i];
 		uint32_t m = FLAG_MAP[c];
 		if (m == 0) {
-			printf("ls: invalid option -- %c", c);
+			ft_printf("ls: invalid option -- %c", c);
 			return false;
 		}
 		tflags->flags_mask |= m;
+		if (flags[i] == 'l' || flags[i] == 's' || flags[i] == 'S' || flags[i] == 'g')
+			tflags->stat = true;
 	}
 	return true;
 }
 
-void initfList(struct flags *flags) {
+void initfList(struct env *flags) {
 	flags->flags_mask = 0;
+	flags->stat = false;
 }
 
-static int	cmp_name(const void *a, const void *b)
+static int	cmpName(const void *a, const void *b)
 {
 	char *ca = ft_strdup(((struct filesData *)a)->name);
 	char *cb = ft_strdup(((struct filesData *)b)->name);
 
 	char *na = ca;
 	char *nb = cb;
-	/* ignorer le point initial */
 	while (*na == '.') na++;
 	while (*nb == '.') nb++;
 
-	/* comparaison case-insensitive */
 	for (int i = 0; na[i]; i++) na[i] = tolower(na[i]);
 	for (int i = 0; nb[i]; i++) nb[i] = tolower(nb[i]);
 	
@@ -74,7 +89,7 @@ static int	cmp_name(const void *a, const void *b)
 }
 
 int main(int ac, char **av) {
-	struct flags	flags;
+	struct env	flags;
 	struct dirent	*readDir;
 	DIR				*dirfile;
 	char			*to_open[ac];
@@ -105,30 +120,30 @@ int main(int ac, char **av) {
 			struct filesData	files[500];
 			int					k = 0;
 
-			if (format == true) printf("%s:\n", to_open[i]);
+			if (format == true) ft_printf("%s:\n", to_open[i]);
 			while ((readDir = readdir(dirfile)) != NULL) {
-				/* skip hidden files when -a not set */
-				if (readDir->d_name[0] == '.' && !flags_is_set(flags.flags_mask, 'a'))
+				if (readDir->d_name[0] == '.' && !flagIsSet(flags.flags_mask, 'a'))
 					continue;
 				strncpy(files[k].name, readDir->d_name, 255);
 				files[k].name[255] = '\0';
 				files[k].type = readDir->d_type;
+				if (flags.stat) {
+					files[k].stat = malloc(sizeof(struct stat));
+					if (stat(files[k].name ,files[k].stat) != 0) {
+						perror("stat error");
+						break;
+					}
+				}
 				k++;
 			}
-			qsort(files, k, sizeof(struct filesData), cmp_name);
-			if (flags_is_set(flags.flags_mask, 'r')) {
-				k-=1;
-				for (; k >= 0; k--)
-					filePrinter(files[k].name, files[k].type);
-			} else
-				for (int m = 0; m < k; m++)
-					filePrinter(files[m].name, files[m].type);
+			qsort(files, k, sizeof(struct filesData), cmpName);
+			filesPrinter(files, flags.flags_mask, k);
 			putchar('\n');
 			if (to_open[i + 1] != NULL)
 				putchar('\n');
 			closedir(dirfile);
 		} else {
-			printf("ls: cannot access '%s': No such file or directory", to_open[i]);
+			ft_printf("ls: cannot access '%s': No such file or directory", to_open[i]);
 		}
 	}
 	return EXIT_SUCCESS;
