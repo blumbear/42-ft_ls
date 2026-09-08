9f996d7dd2e649e4b7f116070e45aaa168008763
Hp de moa <taquettom3@gmail.com>
2026-09-08 19:29:14 +0200

    ~ | increased includes file readability

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -108,11 +108,11 @@ int nbrlen(unsigned int n);
 struct column_max_widths calculate_widths(struct filesData files[], int last) ;
 
 //struct dirent {
-//     ino_t          d_ino;       /* Inode number */
-//     off_t          d_off;       /* Not an offset; see below */
-//     unsigned short d_reclen;    /* Length of this record */
-//     unsigned char  d_type;      /* Type of file; not supported by all filesystem types */
-//     char           d_name[256]; /* Null-terminated filename */
+//	ino_t			d_ino;			/* Inode number */
+//	off_t			d_off;			/* Not an offset; see below */
+//	unsigned short	d_reclen;		/* Length of this record */
+//	unsigned char	d_type;			/* Type of file; not supported by all filesystem types */
+//	char			d_name[256];	/* Null-terminated filename */
 //};
 
 //	type :
@@ -122,17 +122,17 @@ struct column_max_widths calculate_widths(struct filesData files[], int last) ;
 
 
 // struct stat {
-//     dev_t     st_dev;      /* ID du périphérique contenant le fichier */
-//     ino_t     st_ino;      /* Numéro inœud */
-//     mode_t    st_mode;     /* Protection */
-//     nlink_t   st_nlink;    /* Nb liens matériels */
-//     uid_t     st_uid;      /* UID propriétaire */
-//     gid_t     st_gid;      /* GID propriétaire */
-//     dev_t     st_rdev;     /* ID périphérique (si fichier spécial) */
-//     off_t     st_size;     /* Taille totale en octets */
-//     blksize_t st_blksize;  /* Taille de bloc pour E/S */
-//     blkcnt_t  st_blocks;   /* Nombre de blocs alloués */
-//     time_t    st_atime;    /* Heure dernier accès */
-//     time_t    st_mtime;    /* Heure dernière modification */
-//     time_t    st_ctime;    /* Heure dernier changement état */
+//	dev_t		st_dev;		/* ID du périphérique contenant le fichier */
+//	ino_t		st_ino;		/* Numéro inœud */
+//	mode_t		st_mode;	/* Protection */
+//	nlink_t		st_nlink;	/* Nb liens matériels */
+//	uid_t		st_uid;		/* UID propriétaire */
+//	gid_t		st_gid;		/* GID propriétaire */
+//	dev_t		st_rdev;	/* ID périphérique (si fichier spécial) */
+//	off_t		st_size;	/* Taille totale en octets */
+//	blksize_t	st_blksize;	/* Taille de bloc pour E/S */
+//	blkcnt_t	st_blocks;	/* Nombre de blocs alloués */
+//	time_t		st_atime;	/* Heure dernier accès */
+//	time_t		st_mtime;	/* Heure dernière modification */
+//	time_t		st_ctime;	/* Heure dernier changement état */
 // };
